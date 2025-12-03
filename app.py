c153210798258f43b68fa1f281f8329d935847aa
JackScoble <jack.scoble@ada.ac.uk>
2025-12-03 10:50:42 +0000

    add tasks route

--- app.py
+++ app.py
@@ -5,3 +5,7 @@ app = Flask(__name__)
 @app.route('/') # www.domain.com/
 def index():
     return "<h1>Todo Index Page</h1>"
+
+@app.route('/tasks') # www.domain.com/tasks
+def all_tasks():
+    return "<h1>All Tasks Page</h1>"
