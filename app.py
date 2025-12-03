51f25515b8d957a2be1703378dd4244238ef3d33
JackScoble <jack.scoble@ada.ac.uk>
2025-12-03 10:55:31 +0000

    add task details route

--- app.py
+++ app.py
@@ -9,3 +9,7 @@ def index():
 @app.route('/tasks') # www.domain.com/tasks
 def all_tasks():
     return "<h1>All Tasks Page</h1>"
+
+@app.route('/task/<int:task_id>') # www.domain.com/task/1
+def task_detail(task_id):
+    return f"<h1>Task Detail Page for Task ID: {task_id}</h1>"
